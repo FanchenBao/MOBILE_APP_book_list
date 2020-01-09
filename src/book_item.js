c3482147b578ef908bc6b0657dbf30c0dfdd141d
Fanchen Bao <jonathonbao@gmail.com>
2020-01-09 14:38:25 -0500

    Add mock books to list

---
+++ src/book_item.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+class BookItem extends Component {
+  render() {
+    return (
+      <View style={styles.bookItem}>
+        <Image style={styles.cover} source={{uri: this.props.image_url}} />
+        <View style={styles.info}>
+          <Text style={styles.author}>{this.props.author}</Text>
+          <Text style={styles.title}>{this.props.title}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bookItem: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    borderBottomColor: '#AAAAAA',
+    borderBottomWidth: 2,
+    padding: 20,
+  },
+  cover: {
+    flex: 1,
+    height: 150,
+    resizeMode: 'contain',
+    width: 90,
+    // borderColor: 'black',
+    // borderWidth: 2,
+  },
+  info: {
+    flex: 2,
+    alignItems: 'flex-end',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    padding: 20,
+    // borderColor: 'black',
+    // borderWidth: 2,
+  },
+  author: {fontSize: 18},
+  title: {fontSize: 18, fontWeight: 'bold'},
+});
+
+export {BookItem};
--- src/book_list.js
+++ src/book_list.js
@@ -1,50 +1,60 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {BookItem} from './book_item.js';
 
 class BookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        {key: 'a'},
-        {key: 'b'},
-        {key: 'c'},
-        {key: 'd'},
-        {key: 'a longer example'},
-        {key: 'e'},
-        {key: 'f'},
-        {key: 'g'},
-        {key: 'h'},
-        {key: 'i'},
-        {key: 'j'},
-        {key: 'k'},
-        {key: 'l'},
-        {key: 'm'},
-        {key: 'n'},
-        {key: 'o'},
-        {key: 'p'},
-      ],
+      data: mockBooks,
     };
   }
-  _renderItem = data => {
-    return <Text style={styles.row}>{data.item.key}</Text>;
+
+  _renderItem = ({item}) => {
+    return (
+      <BookItem
+        image_url={item.book_image}
+        author={item.author}
+        title={item.title}
+      />
+    );
   };
+
   render() {
     return (
       <View style={styles.container}>
-        <FlatList data={this.state.data} renderItem={this._renderItem} />
+        <FlatList
+          data={this.state.data}
+          renderItem={this._renderItem}
+          keyExtractor={item => `${item.rank}_${item.title}`}
+        />
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  row: {fontSize: 24, padding: 42, borderWidth: 1, borderColor: '#DDDDDD'},
 });
+
+const mockBooks = [
+  {
+    rank: 1,
+    title: 'GATHERING PREY',
+    author: 'John Sandford',
+    book_image:
+      'http://du.ec2.nytimes.com.s3.amazonaws.com/prd/books/9780399168796.jpg',
+  },
+  {
+    rank: 2,
+    title: 'MEMORY MAN',
+    author: 'David Baldacci',
+    book_image:
+      'http://du.ec2.nytimes.com.s3.amazonaws.com/prd/books/9781455586387.jpg',
+  },
+];
 
 export {BookList};
